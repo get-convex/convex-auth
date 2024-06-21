58e9d74780821df1b38868448b820215350d9bbb
Michal Srb <srb@convex.dev>
2024-06-21 18:22:43 +0200

    Fix types

--- test/src/main.tsx
+++ test/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "next-themes";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
-import App from "./App.tsx";
+import { App } from "./App.tsx";
 import "./index.css";
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
