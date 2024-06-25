08818ef37d99395aee664889ffc591ace29e1547
Michal Srb <srb@convex.dev>
2024-06-25 16:43:52 +0200

    Fix lint in components

--- test/src/components/ui/toggle.tsx
+++ test/src/components/ui/toggle.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-refresh/only-export-components */
+
 import * as React from "react"
 import * as TogglePrimitive from "@radix-ui/react-toggle"
 import { cva, type VariantProps } from "class-variance-authority"
