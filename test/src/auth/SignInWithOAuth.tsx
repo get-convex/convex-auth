7ec40ad407b6572141a0e829ff91e9712b1f974e
Michal Srb <srb@convex.dev>
2024-07-08 15:21:45 +0200

    Remove apple for now from the demo UI

--- test/src/auth/SignInWithOAuth.tsx
+++ test/src/auth/SignInWithOAuth.tsx
@@ -1,4 +1,3 @@
-import { SignInWithApple } from "@/auth/oauth/SignInWithApple";
 import { SignInWithGitHub } from "@/auth/oauth/SignInWithGitHub";
 import { SignInWithGoogle } from "@/auth/oauth/SignInWithGoogle";
 
@@ -7,7 +6,6 @@ export function SignInWithOAuth() {
     <div className="flex flex-col min-[460px]:flex-row w-full gap-2 items-stretch">
       <SignInWithGitHub />
       <SignInWithGoogle />
-      <SignInWithApple />
     </div>
   );
 }
