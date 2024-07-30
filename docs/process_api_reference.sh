d2f04b06c974a206212432ec673063ea5afcee7a
Michal Srb <srb@convex.dev>
2024-07-30 20:00:47 +0200

    Fix postprocess after upgrade docs

--- docs/process_api_reference.sh
+++ docs/process_api_reference.sh
@@ -12,10 +12,10 @@ perl -i -pe 's/^##### (Type declaration)/|Field|TS Type\n|---|---/g' pages/api_r
 perl -i -0777 -pe 's/\n###### ([^\n]+)\n\n> \*\*[^*]+\*\*:/|`$1`| /g' pages/api_reference/server.mdx
 
 # Remove type parameters, they're not helpful
-find pages/api_reference -type f -exec perl -i -0777 -pe 's/\n(#+) Type parameters(.(?!###))*//sg' {} +
+find pages/api_reference -type f -exec perl -i -0777 -pe 's/\n(#+) Type Parameters(.(?!###))*//sg' {} +
 
 # Remove complicated signatures, we document them where applicable.
 #
 # Also by rewriting the markdown headings to HTML we prevent Nextra from
 # including them in the RHS TOC sidebar.
-find pages/api_reference -type f -exec perl -i -pe 's/^> (?!(`optional` )?\*\*\w+\*\*: `(?!object)\w+`(\[\])?\n).*\n//; s/^(#+) (Returns|Source|Parameters|Properties|Type declaration|Extends|Overrides)/"<h" . length($1) . " class=\"nx-font-semibold nx-tracking-tight nx-text-slate-900 dark:nx-text-slate-100 nx-mt-8 nx-text-" . (length($1) > 3 ? "lg" : "2xl") . "\">" . $2 . "<\/h" . length($1) . ">"/eg' {} +
+find pages/api_reference -type f -exec perl -i -pe 's/^> (?!(`optional` )?\*\*\w+\*\*: `(?!object)\w+`(\[\])?\n).*\n//; s/^(#+) (Returns|Defined in|Parameters|Properties|Type declaration|Extends|Overrides)/"<h" . length($1) . " class=\"nx-font-semibold nx-tracking-tight nx-text-slate-900 dark:nx-text-slate-100 nx-mt-8 nx-text-" . (length($1) > 3 ? "lg" : "2xl") . "\">" . $2 . "<\/h" . length($1) . ">"/eg' {} +
