7fae869791ba25189414413142097d27ff63f55b
xixixao <xixixao@seznam.cz>
2024-10-20 22:01:02 +0200

    Fix the signin failure test

--- test-nextjs/e2e-tests/signin.spec.ts
+++ test-nextjs/e2e-tests/signin.spec.ts
@@ -1,24 +1,25 @@
 import { test, expect } from "@playwright/test";
 
-// test("signin fails correctly", async ({ page }) => {
-//   await page.goto("/");
+test("signin fails correctly", async ({ page }) => {
+  await page.goto("/");
 
-//   await page.getByRole("link").getByText("Get Started").first().click();
+  await page.getByRole("link").getByText("Get Started").first().click();
 
-//   await page
-//     .getByLabel("Secret")
-//     .fill(
-//       "for the love of all mighty please don't set this as the secret value",
-//     );
+  await page
+    .getByLabel("Secret")
+    .fill(
+      "for the love of all mighty please don't set this as the secret value",
+    );
 
-//   // Record the alert message
-//   let message = "";
-//   page.on("dialog", (dialog) => {
-//     message = dialog.message();
-//     dialog.accept();
-//   });
+  // Record the alert message
+  let message = "";
+  page.on("dialog", (dialog) => {
+    message = dialog.message();
+    dialog.accept();
+  });
 
-//   await page.getByRole("button").getByText("Sign in with secret").click();
+  await page.getByRole("button").getByText("Sign in with secret").click();
 
-//   expect(message).toBe("Invalid secret");
-// });
+  // Need to wait for the dialog to appear, it's async
+  await expect.poll(async () => message).toBe("Invalid secret");
+});
