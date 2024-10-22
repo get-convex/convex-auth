6d6cd7b561c76748525d582c5d375ae3c320ad29
Christian Wyglendowski <christian@dowski.com>
2024-10-22 14:04:34 -0400

    A few more tweaks removing RegEx as valid input types

--- src/nextjs/server/routeMatcher.ts
+++ src/nextjs/server/routeMatcher.ts
@@ -26,24 +26,19 @@ import { pathToRegexp } from "path-to-regexp";
 import type Link from "next/link";
 import type { NextRequest } from "next/server";
 
-type WithPathPatternWildcard<T> = `${T & string}(.*)`;
 type NextTypedRoute<T = Parameters<typeof Link>["0"]["href"]> = T extends string
   ? T
   : never;
 
 type Autocomplete<U extends T, T = string> = U | (T & Record<never, never>);
 
-type RouteMatcherWithNextTypedRoutes = Autocomplete<
-  // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
-  WithPathPatternWildcard<NextTypedRoute> | NextTypedRoute
->;
+type RouteMatcherWithNextTypedRoutes = Autocomplete<NextTypedRoute>;
 
 /**
  * See {@link createRouteMatcher} for more information.
  */
 export type RouteMatcherParam =
-  | Array<RegExp | RouteMatcherWithNextTypedRoutes>
-  | RegExp
+  | Array<RouteMatcherWithNextTypedRoutes>
   | RouteMatcherWithNextTypedRoutes
   | ((req: NextRequest) => boolean);
 
@@ -53,9 +48,9 @@ export type RouteMatcherParam =
  *
  * You can use glob patterns to match multiple routes or a function to match against the request object.
  * Path patterns are supported, for example: `['/foo', '/bar{/*extra}'].
- * 
+ *
  * Note: regular expressions are not supported in path patterns.
- * 
+ *
  * For more information, see: https://github.com/pillarjs/path-to-regexp
  */
 export const createRouteMatcher = (routes: RouteMatcherParam) => {
