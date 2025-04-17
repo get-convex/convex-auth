647f610c28076089e775f4a50cd7c2e3436b2dc8
Micha Mailaender <micha@mailaender.me>
2025-04-17 20:05:43 +0200

    Migrate from AuthProvider to setupConvexAuth

--- test-sveltekit/src/routes/+layout.svelte
+++ test-sveltekit/src/routes/+layout.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
 	import '../app.css';
-	import { createSvelteKitAuthProvider } from '@convex-dev/auth/sveltekit';
+	import { setupConvexAuth } from '@convex-dev/auth/sveltekit';
 
 	let { children, data } = $props();
 
-	const AuthProvider = createSvelteKitAuthProvider();
+	setupConvexAuth({ serverState: data.authState });
 </script>
 
-<AuthProvider serverState={data.authState}>
-	{@render children()}
-</AuthProvider>
+{@render children()}
