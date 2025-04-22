b59acbc8d37c62ed3f119e1e2ee68dcc647e692b
Nicolas Ettlin <nicolas@convex.dev>
2025-04-22 10:50:21 -0700

    Update random string generation in `src`

--- src/server/implementation/utils.ts
+++ src/server/implementation/utils.ts
@@ -1,5 +1,9 @@
 import { sha256 as rawSha256 } from "oslo/crypto";
 import { encodeHex } from "oslo/encoding";
+import {
+  RandomReader,
+  generateRandomString as osloGenerateRandomString,
+} from "@oslojs/crypto/random";
 
 export const TOKEN_SUB_CLAIM_DIVIDER = "|";
 export const REFRESH_TOKEN_DIVIDER = "|";
@@ -10,6 +14,16 @@ export function stringToNumber(value: string | undefined) {
 
 export async function sha256(input: string) {
   return encodeHex(await rawSha256(new TextEncoder().encode(input)));
+}
+
+export function generateRandomString(alphabet: string, length: number) {
+  const random: RandomReader = {
+    read(bytes) {
+      crypto.getRandomValues(bytes);
+    },
+  };
+
+  return osloGenerateRandomString(random, alphabet, length);
 }
 
 export function logError(error: unknown) {
