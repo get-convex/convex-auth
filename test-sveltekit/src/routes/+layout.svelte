476e14576f6bbc3c57a708c84fa6a0789916aeb6
Micha Mailaender <micha@mailaender.me>
2025-04-24 16:30:59 +0200

    Formatted

--- test-sveltekit/src/routes/+layout.svelte
+++ test-sveltekit/src/routes/+layout.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
 	import { setupConvexAuth } from '@convex-dev/auth/sveltekit';
 	import { PUBLIC_CONVEX_URL } from '$env/static/public';
-	
+
 	import '../app.css';
 
 	let { children, data } = $props();
 
-	setupConvexAuth({ serverState: data.authState, convexUrl: PUBLIC_CONVEX_URL });
+	setupConvexAuth({
+		serverState: data.authState,
+		convexUrl: PUBLIC_CONVEX_URL
+	});
 </script>
 
 {@render children()}
