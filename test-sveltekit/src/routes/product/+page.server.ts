0965f6146a0d748fc6fa75595c91360324a38632
Micha Mailaender <micha@mailaender.me>
2025-05-04 16:32:47 +0200

    Simplify auth client creation by using createConvexHttpClient helper

--- test-sveltekit/src/routes/product/+page.server.ts
+++ test-sveltekit/src/routes/product/+page.server.ts
@@ -1,16 +1,11 @@
 import type { PageServerLoad } from './$types';
-import { ConvexHttpClient } from 'convex/browser';
-import { PUBLIC_CONVEX_URL } from '$env/static/public';
 import { api } from '$lib/convex/_generated/api.js';
 import { createConvexAuthHandlers } from '@convex-dev/auth/sveltekit/server';
 
 export const load = (async (event) => {
-    const auth = await createConvexAuthHandlers();
-    const state = await auth.getAuthState(event);
+    const { createConvexHttpClient} = await createConvexAuthHandlers();
+    const client = await createConvexHttpClient(event);
 
-    const client = new ConvexHttpClient(PUBLIC_CONVEX_URL);
-    client.setAuth(state._state.token!)
-    
     const viewer = await client.query(api.users.viewer, {});
     const messages = await client.query(api.messages.list, {});
     return {
