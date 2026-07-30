adccfb53449e006f85eb8fe61620058c088e14eb
Christian Wyglendowski <christian@convex.dev>
2026-07-30 10:12:50 -0400

    Formatting

--- examples/nextjs/proxy.ts
+++ examples/nextjs/proxy.ts
@@ -1,4 +1,7 @@
-import { convexAuthNextjsProxy, nextjsProxyRedirect } from "@/src/lib/convexAuth";
+import {
+  convexAuthNextjsProxy,
+  nextjsProxyRedirect,
+} from "@/src/lib/convexAuth";
 
 // Uses the provided convexAuthNextjsProxy function to build a custom proxy handler
 // for this example application. As seen here, you supply a custom function that makes
